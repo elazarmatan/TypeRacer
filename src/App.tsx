894946f77441b8074899f19fd0d334b4da2caddc
elazar matan <elazarmata8@gmail.com>
2025-11-02 11:15:07 +0200

    ``` feat: Wrap app with TimerProvider.
    This commit wraps the application with the TimerProvider to manage timer state.
    ```

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Layout from "./components/Layout"
+import { TimerProvider } from "./context/timer.context"
 
 function App() {
   return (
     <>
+    <TimerProvider>
     <Layout/>
+    </TimerProvider>
     </>
   )
 }
