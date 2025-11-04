0d35464251c044cf4dd30f99c4d4fef7f0c1aca1
elazar matan <elazarmata8@gmail.com>
2025-11-04 14:51:03 +0200

    feat: Display top 5 leaderboard scores.
    Implements leaderboard display with name and record. Limits to top 5.

--- src/components/leaderboard.tsx
+++ src/components/leaderboard.tsx
@@ -1,9 +1,16 @@
+import { useTimer } from "../context/timer.context"
+import getLeaderboard from "../utils/getLeaderBoard"
 
 function Leaderboard() {
+    const {saveRecord} = useTimer()
+    const leaderboard = getLeaderboard()
+    console.log(saveRecord)
   return (
     <div id="leader">
     <h1>Leaderboard</h1>
-    <ul></ul>
+    <ul>
+        {leaderboard.length && leaderboard.map((leader:{name:string,record:number},index:number)=>  index < 5 &&<li key={leader.name}>{leader.name}:{leader.record}</li>)}
+    </ul>
     </div>
   )
 }
