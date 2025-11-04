d03c39e150a562fb7e3490062a523fec48f4fe09
elazar matan <elazarmata8@gmail.com>
2025-11-04 14:50:36 +0200

    feat: Add record saving functionality
    Implements saving records with user names to the leaderboard.
    
    feat: Prevent space key in input
    
    Prevents space key from being entered in input field.
    
    feat: Show pace after name input
    
    Displays user's words per minute pace after name submission.

--- src/components/RecordSetting.tsx
+++ src/components/RecordSetting.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import keepLeaderboard from "../utils/keepLeaderboard";
+import { useTimer } from "../context/timer.context";
 
 interface props {
   counter: number;
@@ -6,30 +8,40 @@ interface props {
 }
 
 function RecordSetting({ counter, time }: props) {
+    const {setSaveRecord} = useTimer()
   const inputName = useRef<HTMLInputElement | null>(null);
-  const record = ((counter + 1) / time) * 60;
+  const record = ((counter) / time) * 60;
+  const [enterName, setEnterName] = useState(false);
   useEffect(() => {
     inputName.current?.focus();
-  }),
-    [];
+  }, []);
   return (
-    <div id="record">
-      <label htmlFor="name">
-        Enter your name if you want to keep the record.
-      </label>
-      <input
-        type="text"
-        id="name"
-        ref={inputName}
-        placeholder="enter your name"
-        onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
-          if (inputName.current?.value.trim()) {
-            if (e.key === "Enter") {
-              console.log(`name:${inputName.current?.value}`);
+    <div>
+      {!enterName && (
+        <div id="record">
+          <label htmlFor="name">
+            Enter your name if you want to keep the record.
+           </label><input
+            type="text"
+            id="name"
+            ref={inputName}
+            placeholder="enter your name"
+            onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+            if(e.key === ' '){
+                 e.preventDefault()
+                return;
             }
-          }
-        }}
-      />
+              if (inputName.current?.value.trim()) {
+                if (e.key === "Enter") {
+                  keepLeaderboard(record, inputName.current.value);
+                  setEnterName(true);
+                  setSaveRecord(prev => !prev)
+                }
+              }
+            }}
+          />
+        </div>
+      )}
       <p>Your pace is {record} words per minute.</p>
     </div>
   );
