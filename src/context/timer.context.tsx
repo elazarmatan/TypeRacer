7a07ec619e043b639ab2e7d20efb0626b3f1a46a
elazar matan <elazarmata8@gmail.com>
2025-11-09 10:45:00 +0200

    fix: Update context type definition and include missing state in TimerProvider

--- src/context/timer.context.tsx
+++ src/context/timer.context.tsx
@@ -1,6 +1,6 @@
 import { createContext , useContext, useState} from "react";
 
-type TimerContextType = {
+type ContextType = {
   time: number;
   setTime: React.Dispatch<React.SetStateAction<number>>;
   isRunning: boolean;
@@ -9,6 +9,8 @@ type TimerContextType = {
   setCounter:React.Dispatch<React.SetStateAction<number>>;
   setSaveRecord: React.Dispatch<React.SetStateAction<boolean>>;
   saveRecord:boolean
+  correct:boolean
+ setCorrect:React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 interface ThemeProviderProps {
@@ -17,19 +19,20 @@ interface ThemeProviderProps {
 
 
 
-export const context = createContext<TimerContextType | null>(null)
+export const context = createContext<ContextType | null>(null)
 
 export function TimerProvider(props:ThemeProviderProps){
     const [time,setTime] = useState(0)
     const [isRunning,setIsRunning] = useState(false)
     const [counter,setCounter] = useState(0)
     const [saveRecord,setSaveRecord] = useState(false)
+    const [correct, setCorrect] = useState(true);
     
 
-    return <context.Provider value={{time,setTime,isRunning,setIsRunning,counter,setCounter,saveRecord,setSaveRecord}}>{props.children}</context.Provider>
+    return <context.Provider value={{time,setTime,isRunning,setIsRunning,counter,setCounter,saveRecord,setSaveRecord,correct,setCorrect}}>{props.children}</context.Provider>
 }
 
-export function useTimer() {
+export function useMyContext() {
   const timer = useContext(context);
   if (!timer) throw new Error("useTimer must be used within a TimerProvider");
   return timer;
