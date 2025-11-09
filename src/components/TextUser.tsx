14182322866d3f0ea1d2b33f601754640abad093
elazar matan <elazarmata8@gmail.com>
2025-11-09 10:44:41 +0200

    fix: Update context import and adjust rendering logic in TextUser component

--- src/components/TextUser.tsx
+++ src/components/TextUser.tsx
@@ -1,14 +1,14 @@
-import { useTimer } from "../context/timer.context";
+import { useMyContext } from "../context/timer.context";
 
 interface props {
   text:string[]
 }
 
 function TextUser({text}:props) {
-  const {counter,isRunning} = useTimer()
+  const {counter,isRunning} = useMyContext()
   return (
-    <div id="text">
-    {isRunning && text.map((word,index) => (<p key={word + index} className={counter >= index ? 'word-input':''}>{word}</p>))}
+   <div id="text">
+    {isRunning && text.map((word,index) => (<p key={word + index} className={counter === index ? 'word-specific':'words'}>{word}</p>))}
     </div>
   );
 }
